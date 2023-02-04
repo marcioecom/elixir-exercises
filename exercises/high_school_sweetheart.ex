0f7ed601011a547b29412eb70c25c1af22223eeb
Marcio Junior <marcio.mj70@gmail.com>
2023-02-04 15:05:44 -0300

    high school sweetheart exercise

---
+++ exercises/high_school_sweetheart.ex
@@ -0,0 +1,41 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    String.trim(name)
+    |> String.first()
+  end
+
+  def initial(name) do
+    letter = first_letter(name)
+    |> String.upcase()
+
+    "#{letter}."
+  end
+
+  def initials(full_name) do
+    [first_name, last_name] = String.split(full_name)
+
+    initial(first_name) <> " " <> initial(last_name)
+  end
+
+  def pair(full_name1, full_name2) do
+    initials1 = initials(full_name1)
+    initials2 = initials(full_name2)
+
+"""
+    ******       ******
+  **      **   **      **
+**         ** **         **
+**            *            **
+**                         **
+**     #{initials1}  +  #{initials2}     **
+ **                       **
+   **                   **
+     **               **
+       **           **
+         **       **
+           **   **
+             ***
+              *
+"""
+  end
+end
