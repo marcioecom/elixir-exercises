b43df50859796c80c79c4259ff05b32c5803e388
Marcio Junior <marcio.mj70@gmail.com>
2023-02-05 23:16:14 -0300

    take a number exercise

---
+++ exercises/take_a_number.ex
@@ -0,0 +1,21 @@
+defmodule TakeANumber do
+  @first_num 0
+
+  def start(), do: spawn(&handle/0)
+
+  defp handle(num \\ @first_num) do
+    receive do
+      {:report_state, from} ->
+        send(from, num)
+        handle(num)
+
+      {:take_a_number, from} ->
+        send(from, num + 1)
+        handle(num + 1)
+
+      :stop -> Process.exit(self(), :kill)
+
+      _ -> handle(num)
+    end
+  end
+end
