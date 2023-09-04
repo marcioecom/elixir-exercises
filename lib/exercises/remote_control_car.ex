c104b8754b879afaa9289421f16f5da32cfcc937
Márcio Júnior <marcio.mj70@gmail.com>
2023-09-04 13:24:21 -0300

    chore: adjust code to use module alias

--- lib/exercises/remote_control_car.ex
+++ lib/exercises/remote_control_car.ex
@@ -7,26 +7,26 @@ defmodule RemoteControlCar do
   ]
 
   def new(nickname \\ "none") do
-    %RemoteControlCar{nickname: nickname}
+    %__MODULE__{nickname: nickname}
   end
 
-  def display_distance(%RemoteControlCar{distance_driven_in_meters: distance }) do
+  def display_distance(%__MODULE__{distance_driven_in_meters: distance }) do
     "#{distance} meters"
   end
 
-  def display_battery(%RemoteControlCar{battery_percentage: battery }) do
+  def display_battery(%__MODULE__{battery_percentage: battery }) do
     case battery do
       0 -> "Battery empty"
       percentage -> "Battery at #{percentage}%"
     end
   end
 
-  def drive(%RemoteControlCar{battery_percentage: 0} = remote_car) do
+  def drive(%__MODULE__{battery_percentage: 0} = remote_car) do
     remote_car
   end
 
-  def drive(%RemoteControlCar{battery_percentage: battery, distance_driven_in_meters: distance } = remote_car) do
-    %RemoteControlCar{
+  def drive(%__MODULE__{battery_percentage: battery, distance_driven_in_meters: distance } = remote_car) do
+    %__MODULE__{
       remote_car |
       battery_percentage: battery - 1,
       distance_driven_in_meters: distance + 20
