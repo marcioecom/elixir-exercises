73a80d6377c7493b172e6249519a92e7dc1a71af
Márcio Júnior <marcio.mj70@gmail.com>
2023-09-04 14:20:54 -0300

    feat: newsletter exercise

---
+++ lib/exercises/newsletter.ex
@@ -0,0 +1,34 @@
+defmodule Newsletter do
+  def read_emails(path) do
+    File.read!(path)
+    |> String.split("\n", trim: true)
+  end
+
+  def open_log(path) do
+    File.open!(path, [:write])
+  end
+
+  def log_sent_email(pid, email) do
+    IO.puts(pid, email)
+  end
+
+  def close_log(pid) do
+    File.close(pid)
+  end
+
+  def send_newsletter(emails_path, log_path, send_fun) do
+    pid = open_log(log_path)
+
+    emails_path
+    |> read_emails()
+    |> Enum.each(&(if send_fun.(&1) == :ok, do: log_sent_email(pid, &1)))
+
+    close_log(pid)
+  end
+end
+
+Newsletter.send_newsletter(
+  "priv/newsletter/emails.txt",
+  "priv/newsletter/log.txt",
+  fn email -> if email != "marcio@mail.com", do: :ok, else: :error end
+)
