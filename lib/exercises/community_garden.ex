239fa3a63b486deba336b7be9d58513dff38a77f
Márcio Júnior <marcio.mj70@gmail.com>
2023-09-06 15:21:01 -0300

    feat: community garden exercise

---
+++ lib/exercises/community_garden.ex
@@ -0,0 +1,46 @@
+# Use the Plot struct as it is provided
+defmodule Plot do
+  @enforce_keys [:plot_id, :registered_to]
+  defstruct [:plot_id, :registered_to]
+end
+
+defmodule CommunityGarden do
+  def start(opts \\ []) do
+    Agent.start(fn -> {0, []} end, opts)
+  end
+
+  def list_registrations(pid) do
+    Agent.get(pid, fn {_, plots} -> plots end)
+  end
+
+  def register(pid, register_to) do
+    Agent.get_and_update(pid, fn {id, plots} = state ->
+      IO.inspect state
+      plot = new_plot(id+1, register_to)
+
+      {plot, {id + 1, [plot | plots]}}
+    end)
+  end
+
+  def release(pid, plot_id) do
+    Agent.update(pid, fn {id, plots} ->
+      {id, Enum.filter(plots, &(&1.plot_id != plot_id))}
+    end)
+  end
+
+  def get_registration(pid, plot_id) do
+    list_registrations(pid)
+    |> Enum.find({:not_found, "plot is unregistered"}, &(&1.plot_id == plot_id))
+  end
+
+  defp new_plot(id, registered_to), do: %Plot{plot_id: id, registered_to: registered_to}
+end
+
+{_, pid} = CommunityGarden.start()
+CommunityGarden.register(pid, "Marcio")
+plot = CommunityGarden.register(pid, "Gabriel")
+# IO.inspect CommunityGarden.list_registrations(pid)
+CommunityGarden.release(pid, plot.plot_id)
+# IO.inspect CommunityGarden.list_registrations(pid)
+CommunityGarden.register(pid, "Gabriel")
+# IO.inspect CommunityGarden.list_registrations(pid)
