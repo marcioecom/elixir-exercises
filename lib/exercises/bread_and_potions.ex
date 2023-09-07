1a6deac4508aa990b37a35e39a0017c449e5223d
Márcio Júnior <marcio.mj70@gmail.com>
2023-09-07 12:50:58 -0300

    feat: bread and potions exercise

---
+++ lib/exercises/bread_and_potions.ex
@@ -0,0 +1,44 @@
+defmodule RPG do
+  defmodule Character do
+    defstruct health: 100, mana: 0
+  end
+
+  defmodule LoafOfBread do
+    defstruct []
+  end
+
+  defmodule ManaPotion do
+    defstruct strength: 10
+  end
+
+  defmodule Poison do
+    defstruct []
+  end
+
+  defmodule EmptyBottle do
+    defstruct []
+  end
+
+  # Add code to define the protocol and its implementations below here...
+  defprotocol Edible do
+    def eat(item, character)
+  end
+
+  defimpl Edible, for: LoafOfBread do
+    def eat(_item, char) do
+      {nil, %Character{char | health: char.health + 5}}
+    end
+  end
+
+  defimpl Edible, for: ManaPotion do
+    def eat(item, char) do
+      {%EmptyBottle{}, %Character{char | mana: char.mana + item.strength}}
+    end
+  end
+
+  defimpl Edible, for: Poison do
+    def eat(_item, char) do
+      {%EmptyBottle{}, %Character{char | health: 0}}
+    end
+  end
+end
