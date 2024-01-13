5118434f471ccdb4f89c1c14bfecae3db7879080
Márcio Júnior <marcio.mj70@gmail.com>
2024-01-13 12:01:09 -0300

    feat: rpn calculator inspection exercise

---
+++ lib/exercises/rpn_calculator_inspection.ex
@@ -0,0 +1,30 @@
+defmodule RPNCalculatorInspection do
+  def start_reliability_check(calculator, input) do
+    pid = spawn_link(fn -> calculator.(input) end)
+    %{input: input, pid: pid}
+  end
+
+  def await_reliability_check_result(%{pid: pid, input: input}, results) do
+     receive do
+      {:EXIT, ^pid, :normal} -> Map.put(results, input, :ok)
+      {:EXIT, ^pid, _reason} -> Map.put(results, input, :error)
+    after
+      100 -> Map.put(results, input, :timeout)
+    end
+  end
+
+  def reliability_check(calculator, inputs) do
+    old_flag = Process.flag(:trap_exit, true)
+
+    inputs
+    |> Enum.map(&start_reliability_check(calculator, &1))
+    |> Enum.reduce(%{}, &await_reliability_check_result/2)
+    |> tap(fn _ -> Process.flag(:trap_exit, old_flag) end)
+  end
+
+  def correctness_check(calculator, inputs) do
+    inputs
+    |> Enum.map(&Task.async(fn -> calculator.(&1) end))
+    |> Enum.map(&Task.await(&1, 100))
+  end
+end
